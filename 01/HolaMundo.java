ec388e229387249d13a312dc1a56392a6f616ca6
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-23 15:01:33 -0300

    01

---
+++ 01/HolaMundo.java
@@ -0,0 +1,7 @@
+//Mi clase en Java
+public class HolaMundo {
+    public static void main(String args[]){
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+    }
+}
--- 01/HolaMundoJava/src/main/java/HolaMundo.java
+++
@@ -1,6 +0,0 @@
-//Mi clase en Java
-public class HolaMundo {
-    public static void main (String args[]){
-        System.out.println("Hola mundo");
-    }
-}
