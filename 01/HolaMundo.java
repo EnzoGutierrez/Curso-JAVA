5fdf392e984eea79bb65d37de703c2e7b02df6a8
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-24 10:08:52 -0300

    Update HolaMundo.java

--- 01/HolaMundo.java
+++ 01/HolaMundo.java
@@ -1,7 +1,20 @@
-//Mi clase en Java
 public class HolaMundo {
     public static void main(String args[]){
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Hola";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+
+        var miVariableEntera2 = 2;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena2 = "Hola de nuevo";
+        System.out.println("Pero " + miVariableCadena2);
     }
 }
