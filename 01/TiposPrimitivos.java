e79ceb801e5cfd05fb3feaff9cf5bdd45d12b711
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-24 11:37:30 -0300

    Practicas con Tipos primitivos

---
+++ 01/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+public class TiposPrimitivos {
+    public static void main(String args[]){
+        /* Tipos Primitivos enteros: byte, short, int, long */
+
+        //byte numeroByte = 10;
+        System.out.println("Valor minimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte: " + Byte.MAX_VALUE);
+
+        //short numeroShort = 10;
+        System.out.println("Valor minimo byte: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo byte: " + Short.MAX_VALUE);
+    }
+    
+}
