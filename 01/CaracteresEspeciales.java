42af0aca7ddc9c6a8301241483e7b30a552d368a
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-24 11:36:48 -0300

    Practicas con Caracteres especiales

---
+++ 01/CaracteresEspeciales.java
@@ -0,0 +1,17 @@
+public class CaracteresEspeciales {
+    public static void main (String args[]){
+        var nombre = "Enzo";
+
+        System.out.println("Salto de linea: \n" + nombre);
+        System.out.println("");
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("");
+        System.out.println("Retroseso: \b" + nombre);
+        System.out.println("");
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
+
+        
+    }
+}
