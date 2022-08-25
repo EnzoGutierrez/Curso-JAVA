b27d12057237532f199c858f6787bb297c44f29f
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-25 11:41:12 -0300

    Practicas con tipos primitivos

--- 01/TiposPrimitivos.java
+++ 01/TiposPrimitivos.java
@@ -6,9 +6,54 @@ public class TiposPrimitivos {
         System.out.println("Valor minimo byte: " + Byte.MIN_VALUE);
         System.out.println("Valor maximo byte: " + Byte.MAX_VALUE);
 
+        System.out.println();
+
         //short numeroShort = 10;
-        System.out.println("Valor minimo byte: " + Short.MIN_VALUE);
-        System.out.println("Valor maximo byte: " + Short.MAX_VALUE);
+        System.out.println("Valor minimo short: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo short: " + Short.MAX_VALUE);
+
+        System.out.println();
+
+        //int numeroInt = 10;
+        System.out.println("Valor minimo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo int: " + Integer.MAX_VALUE);
+
+        System.out.println();
+
+        //int numeroLong = 10;
+        System.out.println("Valor minimo int: " + Long.MIN_VALUE);
+        System.out.println("Valor maximo int: " + Long.MAX_VALUE);
+
+        System.out.println("\n");
+
+        /* Tipos Primitivos de tipo flotante: float y double */
+
+        //float numeroFloat = 10.0;
+        System.out.println("Valor minimo float: " + Float.MIN_VALUE);
+        System.out.println("Valor maximo float: " + Float.MAX_VALUE);
+
+        System.out.println();
+
+        //float numeroDouble = 10.0;
+        System.out.println("Valor minimo double: " + Double.MIN_VALUE);
+        System.out.println("Valor maximo double: " + Double.MAX_VALUE);
+
+        System.out.println("\n");
+
+        /* Tipos Primitivos de tipo char: char y double */
+
+        char miChar = 'a';
+        System.out.println(miChar);
+
+        System.out.println();
+
+        char miCharUnicode = '\u0025';
+        System.out.println(miCharUnicode);
+
+        
+        char miCharUnicodeN = 37;
+        System.out.println(miCharUnicodeN);
+
     }
     
 }
