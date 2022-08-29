7ff338c4f9f70c92f0d7a8e0d7d623491718d2e5
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-29 12:37:02 -0300

    Practicas con Clases

---
+++ 01/clases/Persona.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class Persona {
+    //Atributos de la clase
+    String nombre;
+    String apellido;
+
+    //Metodos de la clase
+    public void desplegarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
---
+++ 01/clases/PruebaPersona.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+            Persona persona1 = new Persona();
+            persona1.nombre = "Enzo";
+            persona1.apellido = "Gutierrez";
+            persona1.desplegarInformacion();
+    }
+}
