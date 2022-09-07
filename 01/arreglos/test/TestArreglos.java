3aedc8450db661d3dd69c9262f64ec6a56c3ad2a
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-09-07 10:49:37 -0300

    Practicas con arreglos

---
+++ 01/arreglos/test/TestArreglos.java
@@ -0,0 +1,9 @@
+package arreglos.test;
+
+public class TestArreglos {
+    public static void main(String[] args) {
+        int edades[] = new int [3];
+        edades[0] = 10;
+        System.out.println(edades[0]);
+    }
+}
